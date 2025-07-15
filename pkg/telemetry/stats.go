42bd775ff5619d6ca6597f0d2cd9a416d92776dc
Arnab Chakraborty <me@arnab.ch>
2025-07-15 11:10:06 +0530

    Added room bandwidth usage metrics (outgoing/incoming) collection and reporting to Prometheus

--- pkg/telemetry/stats.go
+++ pkg/telemetry/stats.go
@@ -69,6 +69,8 @@ func (t *telemetryService) TrackStats(key StatsKey, stat *livekit.AnalyticsStat)
 			direction = prometheus.Outgoing
 		}
 
+		room := t.getRoomDetails(key.participantID)
+
 		nacks := uint32(0)
 		plis := uint32(0)
 		firs := uint32(0)
@@ -97,16 +99,22 @@ func (t *telemetryService) TrackStats(key StatsKey, stat *livekit.AnalyticsStat)
 				prometheus.RecordJitter(key.country, direction, key.trackSource, key.trackType, stream.Jitter)
 			}
 		}
-		prometheus.IncrementRTCP(key.country, direction, nacks, plis, firs)
-		prometheus.IncrementPackets(key.country, direction, uint64(packets), false)
-		prometheus.IncrementBytes(key.country, direction, bytes, false)
-		if retransmitPackets != 0 {
-			prometheus.IncrementPackets(key.country, direction, uint64(retransmitPackets), true)
-		}
-		if retransmitBytes != 0 {
-			prometheus.IncrementBytes(key.country, direction, retransmitBytes, true)
-		}
+	}
+	prometheus.IncrementRTCP(key.country, direction, nacks, plis, firs)
+	prometheus.IncrementPackets(key.country, direction, uint64(packets), false)
+	prometheus.IncrementBytes(key.country, direction, bytes, false)
+	prometheus.IncrementRoomBytes(direction, room.Name, bytes, false)
+	if retransmitPackets != 0 {
+		prometheus.IncrementPackets(key.country, direction, uint64(retransmitPackets), true)
+	}
+	if retransmitBytes != 0 {
+		prometheus.IncrementBytes(key.country, direction, retransmitBytes, true)
+		prometheus.IncrementRoomBytes(direction, room.Name, retransmitBytes, true)
+	}
 
+	if worker, ok := t.getWorker(key.participantID); ok {
+		worker.OnTrackStat(key.trackID, key.streamType, stat)
+	}
 		if worker, ok := t.getWorker(key.participantID); ok {
 			worker.OnTrackStat(key.trackID, key.streamType, stat)
 		}
